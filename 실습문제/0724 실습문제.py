103f43c15e28dae5d554adb363e5e0dd1ec51584
정민선 <jungminsun@jeongminseons-MacBook-Air.local>
2025-07-25 00:18:36 +0900

    실습0724

--- 실습문제/0724 실습문제.py
+++ 실습문제/0724 실습문제.py
@@ -1,33 +1,33 @@
-'''
+
 ## 도서관 사용자 관리 서비스 - 사전 준비
 
 import requests
 from pprint import pprint as print
 
 # 무작위 유저 정보 요청 경로
-API_URL = 'https://jsonplaceholder.typicode.com/users/1'
+# API_URL = 'https://jsonplaceholder.typicode.com/users/1'
 
 # API 요청
-response = requests.get(API_URL)
+# response = requests.get(API_URL)
 
 # JSON -> dict 데이터 변환
-parsed_data = response.json()
+# parsed_data = response.json()
 
 # 응답 데이터 출력
-print(response)
+# print(response)
 
 # 변환 데이터 출력
-print(parsed_data)
+# print(parsed_data)
 
 # 변환 데이터의 타입
-print(type(parsed_data))
+# print(type(parsed_data))
 
 # 특정 데이터 출력
-print(parsed_data['name'])
-print(parsed_data['username'])
-print(parsed_data['company']['name'])
+# print(parsed_data['name'])
+# print(parsed_data['username'])
+# print(parsed_data['company']['name'])
 
-
+'''
 ## 함수와 제어문 연습
 
 food_list = [
@@ -86,28 +86,28 @@ while i < len(food_list):
 # food_list의 dict 값 출력
 print(food_list)
 
-
+'''
 
 ## 도서관 사용자 관리 서비스 - 데이터 수집
 
 # 10명의 데이터 요청
-i = 0
+# i = 0
 
-for i in range(1, 11) :
-    # 무작위 유저 정보 요청 경로
-    API_URL = (f'https://jsonplaceholder.typicode.com/users/{i}')
+# for i in range(1, 11) :
+#     # 무작위 유저 정보 요청 경로
+#     API_URL = (f'https://jsonplaceholder.typicode.com/users/{i}')
 
-    ## API 요청
-    response = requests.get(API_URL)
+#     ## API 요청
+#     response = requests.get(API_URL)
 
-    # JSON -> dict 데이터 변환
-    parsed_data = response.json()
+#     # JSON -> dict 데이터 변환
+#     parsed_data = response.json()
 
-    # 이름만 출력
-    print(parsed_data['name'])
+#     # 이름만 출력
+#     print(parsed_data['name'])
 
 
-
+'''
 ## 대여 불가 도서 구분하기
 list_of_book = [
     '장화홍련전',
@@ -148,6 +148,8 @@ for book in list_of_book:
 
 '''
 
+'''
+
 ## 중첩 리스트 순회 연습
 
 matrix = [
@@ -161,26 +163,106 @@ matrix = [
 
 matrix_len = 0
 
-for matrix_len in matrix:
+for _ in matrix:
+    matrix_len += 1
+print(matrix_len)
+
+for matrix_len in matrix: 
     temporary_len = 0
-    for j in matrix_len <= 4:
+    for number in matrix_len[:4]:
         temporary_len +=1
     print(f"{matrix_len}리스트는 {temporary_len}개 만큼 요소를 가지고 있습니다.")
 
-# for matrix_len in matrix[:] :
-#     matrix_len += 1
-#     print(matrix_len)
 
-# for number in matrix_len :
-#     temporary_len = 0
-#     for matrix in temporary_len <= 4:
-#         temporary += 1
-#         print(f'{number} 리스트는 {temporary_len}개 만큼 요소를 가지고 있습니다.')
-
-for x in range(matrix_len(x, y)):
+for x in range(len(matrix)):
     for y in range(len(matrix[x])):
-        matrix[x][y]
-    print('matrix의 {x}, {y}번째 요소의 값은 {matrix[x][y]}입니다.')
+        print(f'matrix의 {x}, {y}번째 요소의 값은 {matrix[x][y]}입니다.')
+
+
+
+## 도서관 사용자 관리 서비스 - 데이터 처리 - 몰라.. 질무,,,
+
+i = 0
+
+dummy_data = []
+
+for i in range(1, 11) :
+    # 무작위 유저 정보 요청 경로
+    API_URL = (f'https://jsonplaceholder.typicode.com/users/{i}')
+
+    ## API 요청
+    response = requests.get(API_URL)
+
+    # JSON -> dict 데이터 변환
+    parsed_data = response.json()
+
+    # lat, lng 숫자로 변환
+    lat : float(parsed_data['address']['geo']['lat'])
+    lng : float(parsed_data['address']['geo']['lng'])
+
+    # lat(위도)과 lng(경도)의 조건문
+    if -80 < lat < 80 and -80 < lng < 80:
+        dummy = {"company" : parsed_data['company']['name'], 
+                  "name" : parsed_data['name'], 
+                  "lat" : lat, 
+                  "lng" : lng}
+        
+        dummy_data.append(dummy)
+    
+
+
+print(dummy_data)
+    '''
+
+## 대여 불가 도서 관리하기
+
+# 보유 중인 도서 리스트
+list_of_book = [
+    '장화홍련전',
+    '가락국 신화',
+    '온달 설화',
+    '금오신화',
+    '이생규장전',
+    '만복자서포기',
+    '수성지',
+    '백호집',
+    '원생몽유록',
+    '홍길동전',
+    '장생전',
+    '도문대작',
+    '옥루몽',
+    '옥련몽',
+]
+
+# 대여 예정 도서 리스트
+rental_list = [
+    '장생전',
+    '위대한 개츠비',
+    '원생몽유록',
+    '이생규장전',
+    '데미안',
+    '장화홍련전',
+    '수성지',
+    '백호집',
+    '난중일기',
+    '홍길동전',
+    '만복자서포기',
+]
+
+# 보유하고 있지 않은 도서 리스트
+# 리스트 컴프리헨션 [표현식 for 항목 in 반복가능한객체 if 조건]
+missing_book = [book for book in rental_list if book not in list_of_book]
+
+if len(missing_book) == 0:
+    print('모든 도서가 대여 가능한 상태입니다.')
+else :
+    for book in missing_book:
+        print(f'{book} 을/를 보충하여야 합니다.')
+
+
+    
+
+    
 
 
 
