7e1e8c18bfab4de6b7c3809a80fb41cd0a047bbb
정민선 <jungminsun@jeongminseons-MacBook-Air.local>
2025-08-01 00:20:14 +0900

    실습문제

---
+++ 실습문제/0729실습문제/1447. 합집합 Set, union().py
@@ -0,0 +1,37 @@
+# 아래 함수를 수정하시오.
+def union_sets():
+    return set | set2
+    # return set1.union(set2)
+    # result = set()
+    # result = update(set1)
+    # result = update(set2)
+def union_multiple_sets(*sets):
+    # Tuple에 포함된 요소들의 개수 => len(sets)
+    if len(sets) < 2:
+        print("최소 두개의 셋이 필요합니다")
+        return 
+    else:
+        # 가변인자 sets의 자료형은 tuple이다.
+        # tuple은 iterable이다.
+        result = set()
+        for item in sets:
+            result = result | item
+
+        return result
+
+    # 총 합을 구한다.
+    lst = [1, 2, 3, 4, 5]
+    hap = 0
+    for val in lst:
+        hap = hap + val # 누적
+    print(hap) # 15
+
+
+result = union_sets({1, 2, 3}, {3, 4, 5})
+print(result)  # {1, 2, 3, 4, 5}
+
+result = union_multiple_sets({1, 2}, {3, 4}, {5, 6})
+print(result)  # {1, 2, 3, 4, 5, 6}
+
+result = union_multiple_sets({1, 2})
+# 출력 : 최소 두 개의 셋이 필요합니다
---
+++ 실습문제/0729실습문제/3000. 딕셔너리 메서드2.py
@@ -0,0 +1,34 @@
+data = [
+    {
+        'name': 'galxy flip',
+        'company': 'samsung',
+        'is_collapsible': True,
+    },
+    {
+        'name': 'ipad',
+        'is_collapsible': False
+    },
+    {
+        'name': 'galxy fold',
+        'company': 'samsung',
+        'is_collapsible': True
+    },
+    {
+        'name': 'galxy note',
+        'company': 'samsung',
+        'is_collapsible': False
+    },
+    {
+        'name': 'optimus',
+        'is_collapsible': False
+    },
+]
+
+key_list = ['name', 'company', 'is_collapsible']
+
+# 아래에 코드를 작성하시오.
+
+for k in key_list:
+    for d in data:
+        if data.
+            print()
