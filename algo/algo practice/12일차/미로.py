f7951ebdc701283a27babcf0cd52c70c52668d8e
정민선 <jungminsun@jeongminseons-MacBook-Air.local>
2025-08-21 23:51:08 +0900

    실습

--- algo/algo practice/12일차/미로.py
+++ algo/algo practice/12일차/미로.py
@@ -1,2 +1,49 @@
 from collections import deque
 
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+
+# 입력받기
+for t in range(1, 11):
+    input()
+    arr = [list(map(int, input())) for _ in range(16)]
+
+    
+    visited = [[False] * 16 for _ in range(16)]
+    visited[1][1] = True
+
+    q = deque()
+    q.append((1,1))
+
+    can_reach = False
+
+
+    while q: # 큐에 인자가 있으면
+        r, c = q.popleft()
+
+        for d in range(4):
+            nr, nc = r + dr[d], c + dc[d]
+            if 0 <= nr < 16 and 0 <= nc < 16:
+                if arr[nr][nc] == 0 and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    q.append((nr, nc))
+                elif arr[nr][nc] == 3:
+                    can_reach = True
+                    print(f'#{t} 1')
+                    
+    if can_reach == False:
+        print(f'#{t} 0')
+
+                
+                
+        
+
+
+
+
+        
+
+
+
+
+
---
+++ 백준/1592_영식이와 친구들.py
@@ -0,0 +1,30 @@
+# 한 사람이 공을 M번 받으면 게임 끝(지금 받은 공도 포함)
+# 1번부터 시작
+# 현재 공을 받은 횟수가 홀수 : 시계방향으로 L번째에게
+# 짝수 : 반시계 방향으로 L번째
+# 공을 주고 받은 횟수
+
+N, M, L = map(int, input().split()) # 5, 3, 2
+
+cnt = [0] * N
+cnt_total = 0
+i = 0
+result = True
+
+while result:
+    cnt[i] += 1
+    
+    if cnt[i] == M:
+        result = False
+        break
+
+    cnt_total += 1
+
+    if cnt[i] % 2 == 0:
+        i = (i - L) % N
+        
+    elif cnt[i] % 2 == 1:
+        i = (i + L) % N
+        
+print(cnt_total)
+
