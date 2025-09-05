f0caec0e9ef7ea623caf88759ff4ccf83686e6ab
yjsong2154 <yjsong2153@gmail.com>
2025-09-05 17:35:10 +0900

    실습

--- SSAFY/실습문제/0904 실습문제/최대 상금.py
+++ SSAFY/실습문제/0904 실습문제/최대 상금.py
@@ -1,4 +1,33 @@
 T = int(input())
 
 for t in range(1, T+1):
+    num, ch = map(int, input().split())
+
+    path = []
+    number = [] # ['1', '2', '3']
+    change = 0
+
+    for n in str(num):
+        number.append(n)
+
+    visited = [False] * len(number)
+
+    def recur(cnt):
+        global change
+
+        if cnt == len(number):
+            change += 1
+            print(*path)
+            return
+        
+        for idx in range(len(number)):
+            if visited[idx]:
+                continue
     
+            visited[idx] = True
+            path.append(int(number[idx]))
+            recur(cnt + 1)
+            path.pop()
+            visited[idx] = False
+
+    recur(0)
